71486860ae5b4db8d218328b5ef4a0ca08bdf9f2
Oleksand-Zvirych <oaleks262@gmail.com>
2023-11-07 13:12:49 +0200

    edit

--- index.js
+++ index.js
@@ -1,59 +1,99 @@
-require('dotenv').config();
 const express = require('express');
+const dns = require('dns');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
-const shortid = require('shortid');
-const dns = require('dns');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Database for storing shortened URLs
-const urlDatabase = [];
+// Connect to your MongoDB database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { Schema } = mongoose;
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+// Create a URL model
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: Number,
+});
+const Url = mongoose.model('Url', urlSchema);
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// Utility function to generate a short URL
+async function generateShortUrl() {
+  const urlCount = await Url.countDocuments();
+  return urlCount + 1;
+}
+
+// Utility function to validate URLs
+function isValidURL(string) {
+  const regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([/\w\.-]*)*\/?$/;
+  return regex.test(string);
+}
+
+// Serve the HTML form
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/api/shorturl/new', (req, res) => {
+// Handle URL shortening
+app.post('/api/shorturl/new', async (req, res) => {
   const originalUrl = req.body.url;
-  const id = shortid.generate();
 
-  const urlRegex = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
-  if (!urlRegex.test(originalUrl)) {
+  // Check if the URL is valid
+  if (!isValidURL(originalUrl)) {
     return res.json({ error: 'invalid url' });
   }
 
-  const urlObj = new URL(originalUrl);
-  const host = urlObj.hostname;
-
-  dns.lookup(host, (err) => {
-    if (err) {
-      return res.json({ error: 'invalid url' });
+  try {
+    // Check if the URL is already in the database
+    const existingUrl = await Url.findOne({ original_url: originalUrl });
+    if (existingUrl) {
+      return res.json({ original_url: existingUrl.original_url, short_url: existingUrl.short_url });
     }
 
-    urlDatabase.push({ short_url: id, original_url: originalUrl });
+    // Generate a new short URL
+    const short_url = await generateShortUrl();
 
-    res.json({ original_url: originalUrl, short_url: id });
-  });
-});
+    // Create a new URL document
+    const newUrl = new Url({ original_url, short_url });
 
-app.get('/api/shorturl/:id', (req, res) => {
-  const id = req.params.id;
-  const urlData = urlDatabase.find(item => item.short_url === id);
+    // Save it to the database
+    await newUrl.save();
 
-  if (urlData) {
-    res.redirect(urlData.original_url);
-  } else {
-    res.json({ error: 'No short URL found for the given input' });
+    return res.json({ original_url: newUrl.original_url, short_url: newUrl.short_url });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'server error' });
   }
 });
 
-app.listen(port, function() {
+// Redirect to the original URL
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const short_url = req.params.short_url;
+
+  try {
+    // Find the URL document by short URL
+    const url = await Url.findOne({ short_url });
+
+    if (!url) {
+      return res.json({ error: 'short url not found' });
+    }
+
+    res.redirect(url.original_url);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'server error' });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
