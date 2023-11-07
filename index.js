fa86c2b30505508563008e57c8071839e269b637
Oleksand-Zvirych <oaleks262@gmail.com>
2023-11-07 12:52:41 +0200

    edit

---
+++ index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+const urlDatabase = [];
+
+// Розпарсити JSON в запитах POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/api/shorturl/new', (req, res) => {
+  const originalUrl = req.body.url;
+  const id = shortid.generate();
+
+  // Перевірка, чи вхідна URL-адреса відповідає дійсному формату
+  const urlRegex = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+  if (!urlRegex.test(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Збереження пари скорочена URL - вихідна URL
+  urlDatabase.push({ short_url: id, original_url: originalUrl });
+
+  res.json({ original_url: originalUrl, short_url: id });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+  const entry = urlDatabase.find(item => item.short_url === shortUrl);
+
+  if (!entry) {
+    return res.json({ error: 'short_url not found' });
+  }
+
+  res.redirect(entry.original_url);
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
